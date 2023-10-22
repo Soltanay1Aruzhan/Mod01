c0acda2beb304dc3fb4cd75c968be5298fb94288
Soltanay1Aruzhan <123636392+Soltanay1Aruzhan@users.noreply.github.com>
2023-10-22 16:00:43 +0600

    Add files via upload

---
+++ Mod01/Mod01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите свое Фамилия Имя Отчество: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Приветствую {name}");
+            Console.WriteLine("--------------------");
+            Console.Write("Введите число а: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите число b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Сумма равна - {a + b}");
+        }
+    }
+}
